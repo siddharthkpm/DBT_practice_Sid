db41cea4565a94d15d9a60ffd60f10c3f195e445
Siddharth Korakottai Rajakumar <siddharthk@systechusa.com>
2024-10-21 12:04:20 +0000

    change 1

---
+++ profitpercent.sql
@@ -0,0 +1,16 @@
+SELECT
+    p.product_id,
+    p.product_name,
+    TO_CHAR(s.sale_date, 'YYYY-MM') AS month_year,
+    SUM(s.quantity * s.unit_price) AS sales,
+    SUM(s.quantity) AS quantity,
+    AVG(s.unit_price) AS unit_price,
+    SUM((s.unit_price - p.cost_price) * s.quantity) AS profit
+FROM
+    training.dbt_srajakumar.salesdata s
+JOIN
+    training.dbt_srajakumar.products p ON s.product_id = p.product_id
+GROUP BY
+    p.product_id,
+    p.product_name,
+    TO_CHAR(s.sale_date, 'YYYY-MM')
